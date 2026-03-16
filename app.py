a78a64fa449a0a05776227ae9b981a208393818f
Jana Sivačenko <jana.sivacenko@alza.cz>
2026-03-16 13:29:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ with col_side:
         ],
         default=["ELEKTŘINA: Spotřeba (kWh)", "PLYN: Spotřeba (kWh)", "VODA: Spotřeba (m3)"]
     )
-    analyze_btn = st.button("🚀 SPUSTIT ANALÝZU") uprostřed
+    analyze_btn = st.button("🚀 SPUSTIT ANALÝZU")
 
 with col_main:
     # --- LOGIKA WEBHOOKU ---
